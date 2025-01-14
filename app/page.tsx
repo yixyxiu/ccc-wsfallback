cf6bb1d589ff8e05500997b8531e5c2ced61b3df
aabbcc <iyranly@163.com>
2025-01-14 17:19:16 +0800

    feat: add all the rpc url links

--- app/page.tsx
+++ app/page.tsx
@@ -84,6 +84,41 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex flex-wrap gap-4">
+          <a
+            className="flex gap-4 items-center flex-col sm:flex-row rounded-full px-4 py-2 underline underline-offset-2 text-slate-800"
+            href="https://testnet.ckb.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            测试线路1(ckb.dev)
+          </a>
+          <a
+            className="flex gap-4 items-center flex-col sm:flex-row rounded-full px-4 py-2 underline underline-offset-2 text-slate-800"
+            href="https://testnet.ckbapp.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            测试线路2(ckbapp.dev)
+          </a>
+          <a
+            className="flex gap-4 items-center flex-col sm:flex-row rounded-full px-4 py-2 underline underline-offset-2 text-slate-800"
+            href="https://mainnet.ckb.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            主网线路1(ckb.dev)
+          </a>
+          <a
+            className="flex gap-4 items-center flex-col sm:flex-row rounded-full px-4 py-2 underline underline-offset-2 text-slate-800"
+            href="https://mainnet.ckbapp.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            主网线路2(ckbapp.dev)
+          </a>
+        </div>
+
         <div className="flex gap-4 items-center ">
           <div
             className="flex gap-4 items-center flex-col sm:flex-row rounded-full px-4 py-2 bg-slate-800 text-white"
